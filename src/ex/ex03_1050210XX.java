e789b41afe8494739b49fcb435a7513d127ba0b5
Yingyin <105791120@live.asia.edu.tw>
2016-10-03 15:40:22 +0800

    ex03 105791120 楊鈺瑩

--- src/ex/ex03_1050210XX.java
+++ src/ex/ex03_1050210XX.java
@@ -1,15 +1,26 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 輸入一個字元，轉換其大小寫輸出 (大寫 ->小寫，小寫->大寫)
  * Date: 2016/10/03
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex03_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn= new Scanner(System.in);
+		char ch1=scn.next().charAt(0);
+		if(ch1>='a' && ch1<='z')
+		{
+			System.out.println((char)(ch1-'a'+'A'));
+		}
+		else
+		{
+			System.out.println((char)(ch1-'A'+'a'));
+		}
 	}
 
 }
