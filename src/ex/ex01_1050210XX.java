8ce0beec77a3c112778cf29c2ddb0cc78e753786
Yingyin <105791120@live.asia.edu.tw>
2016-10-03 15:59:42 +0800

    ex01 105791120 楊鈺瑩

--- src/ex/ex01_1050210XX.java
+++ src/ex/ex01_1050210XX.java
@@ -16,7 +16,7 @@ public class ex01_1050210XX {
 		char[] data = str.toCharArray();
 		for(int i=data.length-1;i>=0;i--)
 		{
-			System.out.println(data[i]);
+			System.out.print(data[i]);
 		}
 	}
 
