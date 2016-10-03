ebbe490f061e08f3cf80c5ae4e9c1ab77cc7cea8
Yingyin <105791120@live.asia.edu.tw>
2016-10-03 15:54:32 +0800

    ex05 105791120 楊鈺瑩

--- src/ex/ex05_1050210XX.java
+++ src/ex/ex05_1050210XX.java
@@ -1,15 +1,27 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 輸入一正整數，判斷其奇偶數。
  * Date: 2016/10/03
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex05_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner scn = new Scanner(System.in);
+	long a=scn.nextLong();
+	if(a%2==0)
+	{
+		System.out.println("偶數");
+		
+	}
+	else
+	{
+		System.out.println("奇數");
+	}
 	}
 
 }
