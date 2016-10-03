b8f57a7037586829076209d33d218e8ae8d9a4f2
Yingyin <105791120@live.asia.edu.tw>
2016-10-03 14:49:44 +0800

    ex01 105791120 楊鈺瑩

--- src/ex/ex01_1050210XX.java
+++ src/ex/ex01_1050210XX.java
@@ -1,15 +1,23 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 輸入一個字串，並把它反向輸出。
  * Date: 2016/10/03
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex01_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		char[] data = str.toCharArray();
+		for(int i=data.length-1;i>=0;i--)
+		{
+			System.out.println(data[i]);
+		}
 	}
 
 }
