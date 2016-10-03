69735abdaf718bafdd4a3c26ca2e8fc4a942d14e
Yingyin <105791120@live.asia.edu.tw>
2016-10-03 15:36:04 +0800

    ex06  105791120 楊鈺瑩

--- src/ex/ex06_1050210XX.java
+++ src/ex/ex06_1050210XX.java
@@ -1,18 +1,33 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 讓使用者輸入x，並計算y值輸出，其公式如下：
  * y = 3*x2				若 x < -1
  * y = x3+3*x-3			若 -1 <= x <= 1
  * y = 2*x+3			若 x > 1
  * Date: 2016/10/03
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex06_1050210XX {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Scanner scn = new Scanner(System.in);
+		int x=scn.nextInt();
+		if(x<-1)
+		{
+			System.out.println(3*(x*x));
+		}
+		else if(x>=-1 && x<=1)
+		{
+			System.out.println((Math.pow(x, 3))+(3*(x-3)));
+		}
+		else if(x>1)
+		{
+			System.out.println(2*(x+3));
+		}
+		}
 
 }
